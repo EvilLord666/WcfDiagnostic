75d4412058949bb06472907bec09a513672faf66
Ushakov Michael <um.nix.user@gmail.com>
2019-06-11 01:28:37 +0300

    test refactoring

--- Wcf.Diagnostics/Wcf.Diagnostics.NetCore.Impl.Tests/InterfaceImpl/SimpleLogsCaptureCallbackTests.cs
+++ Wcf.Diagnostics/Wcf.Diagnostics.NetCore.Impl.Tests/InterfaceImpl/SimpleLogsCaptureCallbackTests.cs
@@ -14,7 +14,13 @@ namespace Wcf.Diagnostics.NetCore.Impl.Tests.InterfaceImpl
         public void TestLogsCapture()
         {
             // Create server
-
+            BasicHttpBinding binding = new BasicHttpBinding();
+            binding.Name = "TestService";
+            binding.HostNameComparisonMode = HostNameComparisonMode.StrongWildcard;
+            binding.Security.Mode = BasicHttpSecurityMode.None;
+            ServiceHost serviceHost = new ServiceHost(typeof(TestWcfService), new Uri(BaseUri));
+            serviceHost.AddServiceEndpoint(typeof(ITestWcfService), binding, new Uri(EndpointUri));
+            serviceHost.Open();
             //  Create channel for client
             Binding serviceBinding = new BasicHttpBinding(BasicHttpSecurityMode.None);
             EndpointAddress endpointAddress = new EndpointAddress(EndpointUri);
@@ -23,11 +29,13 @@ namespace Wcf.Diagnostics.NetCore.Impl.Tests.InterfaceImpl
 
             int sessionId = server4Client.LogIn("MyDomain", "admin", "123");
 
-            // todo: umv: add here call to get logs
+            // todo: umv: add here call to get logs and check
 
             server4Client.LogOut(sessionId);
+            serviceHost.Close();
         }
 
-        private const string EndpointUri = "http://0.0.0.0:8123/testwcfservice";
+        private const string BaseUri = "http://0.0.0.0:8123/testwcfservice";
+        private const string EndpointUri = "http://0.0.0.0:8123/testwcfservice/test";
     }
 }
---
+++ Wcf.Diagnostics/Wcf.Diagnostics.NetCore.Impl.Tests/readme.md
@@ -0,0 +1,5 @@
+Для того, чтобы модульный тест работал необходимо выполнить:
+
+netsh http add urlacl url=http://127.0.0.1:8123/testwcfservice user=michael
+
+WCF: https://docs.microsoft.com/en-us/dotnet/framework/wcf/feature-details/how-to-expose-a-contract-to-soap-and-web-clients
